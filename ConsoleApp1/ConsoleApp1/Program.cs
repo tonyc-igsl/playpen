d69d628492b8a0f009e3c76fb54f8b4b35fccf72
jacky.fung <jacky.fung@igsl-group.com>
2019-05-20 16:12:49 +0800

    123

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World - jacky use jacky branch");
+            Console.WriteLine("Hello World");
         }
     }
 }
