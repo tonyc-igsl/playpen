120f2247ca735421bc265ff512ba1e16ea0a5eb5
Tony Cheung <tony.cheung@igsl-group.com>
2019-05-24 14:34:26 +0800

    enabled approval, then commit to master directly

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("slack/github integration test");
+            Console.WriteLine("approval and branch checks");
         }
     }
 }
